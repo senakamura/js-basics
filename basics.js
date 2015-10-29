d6cf9d5c3416feaa392c0b58a64aca6f8ddfd9e4
Sean Nakamura <seanenakamura@gmail.com>
2015-10-28 21:10:02 -1000

    completed name, person, and canDrive

--- basics.js
+++ basics.js
@@ -1,19 +1,21 @@
 /* Create a `myName` variable and assign it a String value */
-
+    var myName = "Sean";
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
-
+    var person = {"name": myName, "age": 24};
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
-
+    function greet(name){
+	console.log('Hello, my name is ' + name);
+     }
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
-
+    var canDrive = person.age > 16;
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
